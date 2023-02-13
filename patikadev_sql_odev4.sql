281ecbc569546dc0984b2ecc449622d9c48082fb
Alper alanyalie <30805005+alperalanyali@users.noreply.github.com>
2023-02-13 17:05:24 +0300

    4.odev eklendi

---
+++ patikadev_sql_odev4.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT replacement_cost FROM FILM; -- 1.SORU
+
+SELECT  COUNT( DISTINCT replacement_cost) FROM film; --2.SORU
+
+
+SELECT COUNT(*) FROM film WHERE title LIKE 'T%' AND rating = 'G'; -- 3.SORU
+
+
+SELECT COUNT(*) FROM country WHERE country LIKE '_____'; -- 4.SORU
+
+
+SELECT COUNT(*) FROM city WHERE city ILIKE '%R%r' ; -- 5.SORU
+
